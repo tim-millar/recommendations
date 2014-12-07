6893f453d0a3721769a2fbd4231d05a99c495ba8
Tim Millar <heteroskedasticity99@gmail.com>
2014-12-07 09:53:30 +0000

    Added the README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Recommendations
+
+## Collaborative filtering project using del.i.cious API
+
+Folder contains:
+
+- recommendations.py, a module containing functions for computing
+  distance metrics, matches and recommendations on preference
+  databases.
+- deliciousrec.py, a module containing functions for building
+  prefernce databases using the del.i.cious API.
+
--- deliciousrec.py
+++ deliciousrec.py
@@ -4,6 +4,7 @@ A delicious link recommender using the delicious API.
 
 from pydelicious import get_popular, get_userposts, get_urlposts
 from collections import defaultdict
+from time import sleep
 
 def initialize_user_dict(tag, count=5):
     """
@@ -16,3 +17,30 @@ def initialize_user_dict(tag, count=5):
             user_dict[user] = {}
     return user_dict
 
+
+def fill_items(user_dict):
+    """
+    Returns: a dict whose keys are users and values are 
+    dicts of link: rating pairs, where 1 stands for having 
+    posted the link, else 0.
+    """
+    all_items = {}
+    for user in user_dict:
+        for _ in range(3):
+            try:
+                posts = get_userposts(user)
+                break
+            except:
+                print("Failed user " + user + ", retrying")
+                sleep(4)
+        for post in posts:
+            url = post['url']
+            user_dict[user][url] = 1.0
+            all_items[url] = 1
+
+    for ratings in user_dict.values():
+        for item in all_items:
+            if item not in ratings:
+                ratings[item] = 0.0
+
+    return all_items
